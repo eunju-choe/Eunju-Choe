71728b198a3afefa6a53030eb4bf4c9a0395b2c1
Eunju Choe <eunju_choe@naver.com>
2024-10-09 22:27:08 +0900

    app.py 2차시 파일로 수정

--- my_flask/app.py
+++ my_flask/app.py
@@ -3,8 +3,16 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello, World!"
+def home():
+    return "Welcome to the Home Page!"
+
+@app.route('/about')
+def about():
+    return "This is the About Page!"
+
+@app.route('/contact')
+def contact():
+    return "This is the Contact Page!"
 
 if __name__ == '__main__':
     app.run(debug=True)
